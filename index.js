5f6a8a1dc1ea53a19c60267b86115773a55b93ea
Alexandra Hernandez <ms.hernandeza1@gmail.com>
2018-04-19 23:01:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ function theBeatlesPlay(musicians, instruments) {
   var newArr = [];
   
   for(var i = 0; i < musicians.length; i++) {
-    
+    for (var j = 0; j < instruments.length; j++) {
+      newArr.push(`${musicians[i]} plays ${instruments[j]`);
+    }
   }
 }
